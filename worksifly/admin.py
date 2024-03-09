73d42be7ae495c0e0a4787f26a9c789efdb94f96
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-03-09 15:56:02 +0000

    create drafts, approve comments und manage post

--- worksifly/admin.py
+++ worksifly/admin.py
@@ -5,18 +5,24 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(SecurityFeature)
 class SecurityFeatureAdmin(SummernoteModelAdmin):
     """Allows admin to manage security_feature via the admin panel"""
-    list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'description')
-    #prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     """Allows admin to manage comments on recipes via the admin panel"""
-#     list_display = ('name', 'body', 'security_feature', 'created_on')  # Fix typo here
-#     list_filter = ('created_on',)
-#     search_fields = ('name', 'email', 'body')
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Allows admin to manage comments on recipes via the admin panel"""
+    list_display = ('get_name', 'body', 'security_feature', 'created_on')  
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def get_name(self, obj):
+        return obj.name
+    def approve_comments(self, recipes, queryset):
+        queryset.update(approved=True)
 
 # @admin.register(TechSecurityItem)
 # class TechSecurityItemAdmin(admin.ModelAdmin):
--- worksifly/models.py
+++ worksifly/models.py
@@ -4,6 +4,8 @@ from cloudinary.models import CloudinaryField
 from autoslug import AutoSlugField
 from django.urls import reverse
 from .validators import textfield_not_empty
+from django.utils.text import slugify
+
 
 
 
@@ -14,20 +16,19 @@ STATUS = ((0, "Save for later"), (1, "Publish Now"))
 class SecurityFeature(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    slug = AutoSlugField(populate_from='title', unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="security_features")
     created_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
-    preparation_time = models.IntegerField(default=0)
-    cook_time = models.IntegerField(default=0)
     description = models.TextField()
-    ingredients = models.TextField(validators=[textfield_not_empty])
     method = models.TextField(validators=[textfield_not_empty])
     image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=1)
     bookmarks = models.ManyToManyField(
         User, related_name='bookmarks', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)    
 
     class Meta:
         """To display the security features by created_on in descending order"""
@@ -72,8 +73,10 @@ class Comment(models.Model):
     security_feature = models.ForeignKey(
         SecurityFeature, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         """To display the comments by created_on in ascending order"""
