3cc55b031f64eebb2b825f9252bc73c86f16a6c1
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-04-01 09:30:59 +0000

    email settings

--- worksifly/views.py
+++ worksifly/views.py
@@ -1,6 +1,6 @@
 """Views"""
 
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
